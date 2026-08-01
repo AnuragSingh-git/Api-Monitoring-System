71cf0f53d6da04435636f118c0e55e98dc40cb9c
Anurag Singh <anurag28singh88@gmail.com>
2026-08-01 23:25:57 +0530

    only one db connection functionality

--- my-app/lib/db.ts
+++ my-app/lib/db.ts
@@ -1,14 +1,21 @@
 import mongoose from "mongoose";
 
-export default const connectdb= async():Promise<void>=>{
+const key=process.env.MONGODB_KEY
+
+if(!key){
+    throw new Error("Non key found")
+}
+
+const connectdb= async():Promise<void>=>{
     if(mongoose.connections[0].readyState){
         return
     }
     try {
-        await mongoose.connect(MONGODB_KEY)
+        await mongoose.connect(key)
         console.log("Database Connected")
     } catch (error) {
         console.log("Connection error",error)
         throw error
     }
 }
+export default connectdb
