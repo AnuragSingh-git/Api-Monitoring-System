58c03c1b05b7eae971a01e503629481aaa322373
Anurag Singh <anurag28singh88@gmail.com>
2026-08-07 23:56:06 +0530

    session settings

--- my-app/lib/auth.ts
+++ my-app/lib/auth.ts
@@ -1,7 +1,17 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import connectdb from "./db";
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.MONGODB_KEY!);
 
 export const auth = betterAuth({
-    database: mongodbAdapter(connectdb),   
+    database: mongodbAdapter(client.db("Api")),
+    emailAndPassword: {
+        enabled: true
+    },
+    session: {
+        expiresIn: 60 * 60 * 24 * 7,
+        updateAge: 60 * 60 * 24,
+    }
 });
