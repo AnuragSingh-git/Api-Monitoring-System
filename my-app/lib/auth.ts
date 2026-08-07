615c0ac25dac734946b938256cb2e79a5f2b0bc0
Anurag Singh <anurag28singh88@gmail.com>
2026-08-07 23:41:05 +0530

    connecting database to better auth

--- my-app/lib/auth.ts
+++ my-app/lib/auth.ts
@@ -1,5 +1,7 @@
 import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import connectdb from "./db";
 
 export const auth = betterAuth({
-  //...
+    database: mongodbAdapter(connectdb),   
 });
