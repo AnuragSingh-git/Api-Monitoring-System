e89df97ba338a8062d522ea12ef84925fd7fd395
Anurag Singh <anurag28singh88@gmail.com>
2026-08-17 23:32:36 +0530

    session logic fixed

--- my-app/services/api.services.ts
+++ my-app/services/api.services.ts
@@ -1,6 +1,6 @@
 import api from "../models/api.model";
 import connectdb from "@/lib/db";
-import auth from "../lib/auth";
+import {auth} from "../lib/auth";
 
 connectdb();
 
@@ -15,7 +15,7 @@ interface CreateApiInput {
 
 export const createApi = async (apiData: CreateApiInput) => {
   try {
-    const session = await auth.getSession(); // Replace with actual session retrieval logic
+    const session = await auth.api.getSession();
     if (!session) {
       throw new Error("User not authenticated");
     }
