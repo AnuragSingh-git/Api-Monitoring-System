b4855ce0d36c798a30aac8796db8f1e2bc318412
AnuragSingh-git <Anurag28singh88@gmail.com>
2026-09-01 21:19:41 +0530

    Update get-dashboard.action.ts

--- my-app/actions/api/get-dashboard.action.ts
+++ my-app/actions/api/get-dashboard.action.ts
@@ -11,7 +11,7 @@ export const getDashboardAction = async () => {
       data,
     };
   } catch (error) {
-    console.error("Dashboard action error:", error);
+    console.error("Dashboard actions error:", error);
 
     return {
       success: false,
