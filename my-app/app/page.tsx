46251804770495893fdfefe7e5e1cae9b2b28cac
Anurag Singh <anurag28singh88@gmail.com>
2026-09-08 22:44:46 +0530

    root page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <div>Homepage check
       <p>This is a simple homepage.</p>
+      <p>Welcome to the API Monitoring System!</p>
     </div>
   );
 }
