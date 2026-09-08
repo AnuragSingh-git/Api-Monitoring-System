5565f820d70d1892da2e08e60b2a60d01dd34ea7
Anurag Singh <anurag28singh88@gmail.com>
2026-09-08 22:44:01 +0530

    root page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -4,6 +4,8 @@ import connectdb from "@/lib/db";
 export default function Home() {
   connectdb()
   return (
-    <div>Homepage check</div>
+    <div>Homepage check
+      <p>This is a simple homepage.</p>
+    </div>
   );
 }
